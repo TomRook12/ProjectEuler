242892d1a9372cd211b8a7cfffbb4b448ee1b5a9
Tom Rook <93784825+TomRook12@users.noreply.github.com>
2021-12-06 21:13:25 +0000

    New comparison operation

--- Number4.py
+++ Number4.py
@@ -1,17 +1,30 @@
 '''Hardest, a function to recognise a palindrome,
 Start with code example, 91*99= 9009'''
 
-x = str(9009)
-print(x[0])
-print(x[-1])
+x = "9009"
 
-for i in range(10, 99):
-    break
+mylist = [i in range(10,99)]
+print(mylist)
 
-while x[0] == x[-1]:
+palindrome = False
+test = ''
+
+while chop1 != chop2:
+    for i in range(10,99):
+        test = str(i*i)
+        chop1 = test[:len(test)//2]
+        chop2 = test[len(test)//2:]
+        if chop1 == chop2:
+            print(test)
+            print("Palindrome")
+        else:
+            continue
+
+
+'''if x[0] == x[-1]:
     if x[1] == x[-2]:
         palindrome = True
-    if palindrome == True:
         print("PALINDROME")
-        break
+'''
 
+s = "string"
